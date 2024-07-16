4ad44a19d3eab73b3a5865feed936812a60efdcb
CorderuN <corderun@gmail.com>
2024-07-16 11:03:38 +0300

    Добавлена проверка на наличие кейса в директории Добавлена директория с кейсами

--- src/main/java/org/corderun/meltarionCase/CaseCommands.java
+++ src/main/java/org/corderun/meltarionCase/CaseCommands.java
@@ -1,10 +1,12 @@
 package org.corderun.meltarionCase;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.util.Objects;
 
 public class CaseCommands implements CommandExecutor {
@@ -27,7 +29,28 @@ public class CaseCommands implements CommandExecutor {
                 sender.sendMessage(Objects.requireNonNull(plugin.langConfig.getString("no-perm")).replace("&", "§"));
                 return true;
             }
-
+            if(args.length == 1){
+                sender.sendMessage(Objects.requireNonNull(plugin.langConfig.getString("case.create.usage")).replace("&", "§"));
+                return true;
+            }
+            String caseName = args[1];
+            // Тут хранятся все кейсы
+            File directory = new File("plugins/MeltarionCase/cases");
+            if (directory.exists() && directory.isDirectory()) {
+                File[] files = directory.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        if (file.isFile()) {
+                            String fileName = file.getName();
+                            // Тут кароч проверяется название кейса через названия файлов
+                            if (fileName.equalsIgnoreCase(caseName)) {
+                                sender.sendMessage(Objects.requireNonNull(plugin.langConfig.getString("case.create.already-used")).replace("&", "§"));
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
         }
         return true;
     }
