3d5c256f03834b2194ed88a52441c24738c339ce
CorderuN <corderun@gmail.com>
2024-07-16 13:40:18 +0300

    Добавлена привязка кейса к локации файла Добавлена задержка при нажатии на кейс

--- src/main/java/org/corderun/meltarionCase/EventListener.java
+++ src/main/java/org/corderun/meltarionCase/EventListener.java
@@ -1,15 +1,21 @@
 package org.corderun.meltarionCase;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.io.File;
+import java.util.Objects;
+
 public class EventListener implements Listener {
 
     MeltarionCase plugin;
+    private boolean cdCase = false;
 
     public EventListener(MeltarionCase plugin) {
         super();
@@ -17,15 +23,30 @@ public class EventListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerInteractCase(PlayerInteractEvent e){
+    public void onPlayerInteractCase(PlayerInteractEvent e) {
         Player player = e.getPlayer();
-        Block caseBlock = e.getClickedBlock();
         if (e.getClickedBlock() == null || !e.getAction().name().contains("RIGHT_CLICK_BLOCK")) {
             return;
         }
-        if(e.getClickedBlock().getType().equals(Material.DIAMOND_BLOCK)){
-            plugin.openCase(player);
+        File directory = new File("plugins/MeltarionCase/cases");
+        if (directory.exists() && directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        YamlConfiguration caseFile = YamlConfiguration.loadConfiguration(file);
+                        Location caseLoc = caseFile.getLocation("Location");
+                        if (caseLoc != null && caseLoc.equals(e.getClickedBlock().getLocation())) {
+                            if(!cdCase){
+                                cdCase = true;
+                                plugin.openCase(player);
+                                plugin.getServer().getScheduler().runTaskLater(plugin, () -> cdCase = false, 20L);
+                            }
+                        }
+                    }
+                }
+            }
         }
-    }
 
+    }
 }
