bb10eebf038b9f7b09a6b7594c491f012cc1d353
CorderuN <corderun@gmail.com>
2024-07-16 08:07:00 +0300

    Add command /case Add lang file

---
+++ src/main/java/org/corderun/meltarionCase/CaseCommands.java
@@ -0,0 +1,26 @@
+package org.corderun.meltarionCase;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class CaseCommands implements CommandExecutor {
+
+    MeltarionCase plugin;
+
+    public CaseCommands(MeltarionCase plugin) {
+        this.plugin = plugin;
+    }
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(args.length == 0){
+            sender.sendMessage(Objects.requireNonNull(plugin.langConfig.getString("case.usage")).replace("&", "§"));
+        }
+        return false;
+    }
+}
--- src/main/java/org/corderun/meltarionCase/MeltarionCase.java
+++ src/main/java/org/corderun/meltarionCase/MeltarionCase.java
@@ -1,18 +1,35 @@
 package org.corderun.meltarionCase;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class MeltarionCase extends JavaPlugin {
+
+
+    public YamlConfiguration langConfig;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new EventListener(this), this);
+        this.getCommand("case").setExecutor(new CaseCommands(this));
+        createLangConfig();
     }
 
     public void openCase(Player player){
         player.sendMessage("Открытие кейса...");
+    }
+
+    private void createLangConfig() {
+        File file = new File(getDataFolder(), "lang.yml");
+        if (!file.exists()) {
+            saveResource("lang.yml", false);
+        }
+
+        langConfig = YamlConfiguration.loadConfiguration(file);
     }
 
     @Override
