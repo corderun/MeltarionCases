3129fdd532becb68b5c5a1b817d6bdacad3704e3
CorderuN <corderun@gmail.com>
2024-07-16 11:19:46 +0300

    Добавлена подгрузка папки кейсов

--- src/main/java/org/corderun/meltarionCase/MeltarionCase.java
+++ src/main/java/org/corderun/meltarionCase/MeltarionCase.java
@@ -1,10 +1,15 @@
 package org.corderun.meltarionCase;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Objects;
 
 public final class MeltarionCase extends JavaPlugin {
@@ -18,6 +23,7 @@ public final class MeltarionCase extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new EventListener(this), this);
         Objects.requireNonNull(this.getCommand("case")).setExecutor(new CaseCommands(this));
         createLangConfig();
+        createCaseFolder();
     }
 
     public void openCase(Player player){
@@ -31,6 +37,22 @@ public final class MeltarionCase extends JavaPlugin {
         }
 
         langConfig = YamlConfiguration.loadConfiguration(file);
+    }
+
+    private void createCaseFolder(){
+        File casesDirectory = new File(getDataFolder(), "cases");
+        if (!casesDirectory.exists()) {
+            casesDirectory.mkdirs();
+        }
+        InputStream inpStream = getResource("cases/example.yml");
+        if (inpStream != null) {
+            File exampleYmlFile = new File(casesDirectory, "example.yml");
+            try {
+                Files.copy(inpStream, exampleYmlFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
