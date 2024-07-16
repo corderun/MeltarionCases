1c97a3a2b2f6ac8142383c347ae1b567d5d4001b
CorderuN <corderun@gmail.com>
2024-07-16 07:16:22 +0300

    Initial commit

---
+++ src/main/java/org/corderun/meltarionCase/MeltarionCase.java
@@ -0,0 +1,17 @@
+package org.corderun.meltarionCase;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MeltarionCase extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
